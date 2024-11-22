5ae08238ff71c3419cd7b447fb0e70e8e22977c1
csvidit <63170797+csvidit@users.noreply.github.com>
2024-11-22 10:45:26 -0500

    (tailwind.config.ts) add data directory for parsing, start adding custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,24 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/data/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        linen: {
+          "50": "#f6f6f0",
+          "100": "#e1ddc9",
+          "200": "#d5ceb3",
+          "300": "#bdb087",
+          "400": "#a99766",
+          "500": "#9a8558",
+          "600": "#846c4a",
+          "700": "#6b543d",
+          "800": "#5b4838",
+          "900": "#503f33",
+          "950": "#2d221b",
+        },
       },
     },
   },
