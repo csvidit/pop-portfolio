1a2bb3f2771b06dece919463a2eb54cdc4fd3e7f
csvidit <63170797+csvidit@users.noreply.github.com>
2024-11-25 15:48:55 -0500

    (Header.tsx) update css

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="flex flex-row gap-4 lg:flex-col w-full lg:w-1/6 h-full lg:h-screen sticky">
+    <header className="flex flex-row gap-4 lg:flex-col w-screen lg:w-1/6 h-full lg:h-screen sticky">
       <Nav />
     </header>
   );
