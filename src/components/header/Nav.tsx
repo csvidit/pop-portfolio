ac5846a4f66773b06bd508ea31a72398cb0513b5
csvidit <63170797+csvidit@users.noreply.github.com>
2024-11-25 15:48:47 -0500

    (Nav.tsx) update css

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -3,7 +3,7 @@ import NavItem from "./NavItem";
 
 const Nav = () => {
   return (
-    <nav>
+    <nav className="w-screen lg:w-full">
       <ul className="flex flex-row lg:flex-col gap-4 w-full">
         {nav_data.map((item) => (
           <NavItem key={item.id} {...item} />
