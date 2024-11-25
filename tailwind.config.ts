fb44221a7b243221e6aae7d0d0dd300d1d3afc1f
csvidit <63170797+csvidit@users.noreply.github.com>
2024-11-25 16:11:51 -0500

    (config files) linting changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,30 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/data/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        linen: {
-          "50": "#f6f6f0",
-          "100": "#e1ddc9",
-          "200": "#d5ceb3",
-          "300": "#bdb087",
-          "400": "#a99766",
-          "500": "#9a8558",
-          "600": "#846c4a",
-          "700": "#6b543d",
-          "800": "#5b4838",
-          "900": "#503f33",
-          "950": "#2d221b",
-        },
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/data/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				linen: {
+					"50": "#f6f6f0",
+					"100": "#e1ddc9",
+					"200": "#d5ceb3",
+					"300": "#bdb087",
+					"400": "#a99766",
+					"500": "#9a8558",
+					"600": "#846c4a",
+					"700": "#6b543d",
+					"800": "#5b4838",
+					"900": "#503f33",
+					"950": "#2d221b",
+				},
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
